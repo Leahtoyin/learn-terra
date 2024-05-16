c7fca84f953f9ffdd5cb94cca6c840de8851915d
Leahtoyin <168133423+Leahtoyin@users.noreply.github.com>
2024-05-16 22:36:06 +0900

    edit main

--- main.tf
+++ main.tf
@@ -18,4 +18,19 @@ resource "aws_instance" "app_server" {
     tags = {
     Name = "exampleserverinstance" 
     }
+
+    resource "aws_instance" "example" {
+      ami = "ami-0c7212cdde317cfectia"
+      instance_type = "t2.micro"
+      key_name = "terraform.aws"
+      vpc_security_group_ids = ["sg-0c51540c60857b7ed""sg-0c51540c60857b7ed"]
+      subnet_id = "subnet-096d45c28d9fb4c14"
+      root_block_device{
+        volume_size = "10"
+      }
+      tags = {
+        Name = "vm"
+        create_by = "toyin"
+      }
+    }
 }
